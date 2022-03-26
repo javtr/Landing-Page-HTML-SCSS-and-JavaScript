f28465e7e60ed9f546c11557b03b32e0caa351a9
javtr <javtriana.z@gmail.com>
2022-03-26 10:56:15 -0500

    end-slider

--- app/js/slider.js
+++ app/js/slider.js
@@ -3,9 +3,12 @@ const imgButton1 = document.querySelector("#item1");
 const imgButton2 = document.querySelector("#item2");
 const imgButton3 = document.querySelector("#item3");
 const imgButton4 = document.querySelector("#item4");
+const paybutton = document.querySelector("#play");
 
 let speed = 2000;
 let cont = 0;
+let move = true;
+
 
 let imgUrl = [
   {url: "../../images/chart-1080-720.png"},
@@ -16,14 +19,32 @@ let imgUrl = [
 
 imgButton1.addEventListener("click", () => {
   principal.src = "../../images/chart-1080-720.png";
+  move = false;
+  paybutton.classList.remove("hide");
+
 });
 
 imgButton2.addEventListener("click", () => {
   principal.src = "../../images/gifPruebas.gif";
+  move = false;
+  paybutton.classList.remove("hide");
 });
 
 
+paybutton.addEventListener("click", () => {
+  move = true;
+  cont=0;
+  paybutton.classList.add("hide");
+  console.log("click");
+});
+
+
+
 moveslider = () => {
+
+  if (move) {
+    
+
   principal.src = imgUrl[cont].url;
   cont++;
 
@@ -31,8 +52,22 @@ moveslider = () => {
     cont=0;
   }
 
+}
+
+
   setTimeout(moveslider, speed);
+
+
 };
+
+
+
+moveslider();
+
+
+
+// ========================================
+
 
 // carousel = document.querySelector(".buy__carousel-conten");
 // carouselImages = document.querySelectorAll(".buy__carousel-conten img");
@@ -85,5 +120,5 @@ moveslider = () => {
 // });
 
 // if (move) {
-moveslider();
+// moveslider();
 // }
